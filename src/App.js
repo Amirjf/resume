dba26e7a3e3a8c11e712615149bc399c392ad972
Amirjf <amirmasoodjafari1677@gmail.com>
2021-10-06 13:45:12 +0330

    typewriter effect completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import AvatarInfo from './components/AvatarInfo'
-import PersonInfo from './components/PersonInfo'
-import Devider from './components/Devider'
-import Languages from './components/Languages'
-import Coding from './components/Coding'
-import Knowledge from './components/Knowledge'
-import CounterSection from './components/CounterSection'
-import Services from './components/Services';
+import "./App.css";
+import Typewriter from "typewriter-effect";
+import AvatarInfo from "./components/AvatarInfo";
+import PersonInfo from "./components/PersonInfo";
+import Devider from "./components/Devider";
+import Languages from "./components/Languages";
+import Coding from "./components/Coding";
+import Knowledge from "./components/Knowledge";
+import CounterSection from "./components/CounterSection";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -14,64 +15,75 @@ function App() {
       <div className="wrapper">
         <div className="container-fluid">
           <div className="row p-0 no-gutters">
-          <div className="col-md-3">
-            <div className="side-info">
-              <AvatarInfo />
-              <div className="sidebar">
-              <PersonInfo />
-              <Devider />
-              <Languages />
-              <Devider />
-              <Coding />
-              <Devider />
-              <Knowledge />
+            <div className="col-md-3">
+              <div className="side-info">
+                <AvatarInfo />
+                <div className="sidebar">
+                  <PersonInfo />
+                  <Devider />
+                  <Languages />
+                  <Devider />
+                  <Coding />
+                  <Devider />
+                  <Knowledge />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-md-8">
-          <div className="top-bg-content">
-            <div className="top-bg-overlay"></div>
-            </div>
-            <div className="hero mt-5">
-              {/* <div className="back-shape">
+            <div className="col-md-8">
+              <div className="top-bg-content">
+                <div className="top-bg-overlay"></div>
+              </div>
+              <div className="hero mt-5">
+                {/* <div className="back-shape">
                 
               </div> */}
 
-              <div className="hero-overlay">
-                <div className="hero-title">
-                 <div className="hero-title-h">
-                 <h1 className="text-white">
-                    <span>Hello there my Lovely Employer who wants to hire me</span>
-                   </h1>
-                   </div> 
-                <div className="hero-code">
-                  <h4>code das;lks;lad;kl</h4>
-                </div>
-                </div>
-                <img className="my-pic" src="http://amir-jf.ir/wp-content/uploads/2021/10/face-2-min-1.png" />
-
-              </div>
-            </div>
-            <div className="content">
-                  <div className="container-fluid">
-                    <div className="row">
-                  <CounterSection />
-                  <Services />
+                <div className="hero-overlay">
+                  <div className="hero-title">
+                    <div className="hero-title-h">
+                      <h1 className="text-white">
+                        <span>Hello there{<br />}I`m AmirMasoud</span>
+                      </h1>
+                    </div>
+                    <div className="hero-code">
+                      {"<"}
+                      <i>code</i>
+                      {"> i build "}
+                      <span className="type-writer">
+                        <Typewriter
+                          options={{
+                            strings: ["Web Applications", "Design Websites","Wordpress Websites"],
+                            autoStart: true,
+                            loop: true,
+                          }}
+                        />
+                      </span>
+                      {"<"}
+                      <i>code</i>
+                      {">"}
                     </div>
                   </div>
-                
+                  <img
+                    className="my-pic"
+                    src="http://amir-jf.ir/wp-content/uploads/2021/10/face-2-min-1.png"
+                  />
+                </div>
+              </div>
+              <div className="content">
+                <div className="container-fluid">
+                  <div className="row pt-5">
+                    <CounterSection />
+                    <Services />
+                  </div>
+                </div>
+              </div>
             </div>
-           
-          </div>
             <div className="col-auto"></div>
           </div>
-
         </div>
-
       </div>
     </div>
-
   );
 }
 
